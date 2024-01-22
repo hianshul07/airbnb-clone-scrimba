58a101a9e033a770c5ef481c2e4cbd659427e1a0
hianshul07 <hianshul07@gmail.com>
2024-01-22 23:49:29 +0530

    the project is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import NavBar from "./components/NavBar"
-import './App.css'
-import Hero from "./components/Hero"
-import Card from "./components/Card"
+import NavBar from './components/NavBar';
+import './App.css';
+import Hero from './components/Hero';
+import data from './data';
+import Card from './components/Card';
 
 const App = () => {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        // img={item.coverImg}
+        // rating={item.stats.rating}
+        // reviewCount={item.stats.reviewCount}
+        // location={item.location}
+        // title={item.title}
+        // // description={item.description}
+        // price={item.price}
+        // spots={item.openSpots}
+        item={item}
+      />
+    );
+  });
+
   return (
     <div>
       <NavBar />
-      {/* <Hero /> */}
-      <Card />
+      <Hero />
+      {/* <Card /> */}
+      <section className='card-list'>{cards}</section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,44 @@
-import profilePhoto from '../images/katie-zaferes.png'
+/* eslint-disable react/prop-types */
+// import profilePhoto from '../images/katie-zaferes.png';
+import starPhoto from '../images/star.png';
+import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
+  let badgeText;
+  if (props.item.spots === 0) {
+    badgeText = 'Sold Out';
+  } else if (props.item.location === 'Online') {
+    badgeText = 'Online';
+  }
   return (
-    <div>
-      <img src={profilePhoto} alt="" />
-    </div>
-  )
-}
-export default Card
+    // <div className='card'>
+    <section className=''>
+      {badgeText && <div className='card-badge'>{badgeText}</div>}
+      <img
+        className='profile-photo'
+        src={`./user-images/${props.item.coverImg}`}
+        alt='profile photo'
+      />
+      <div className='profile-info'>
+        <div className='rating'>
+          <img
+            className='star-photo'
+            src={starPhoto}
+            alt='Photo of a red star'
+          />
+          <p>
+            {props.item.stats.rating} ({props.item.stats.reviewCount}) . {props.item.location}
+          </p>
+        </div>
+        <p className='tagline'>{props.item.title}</p>
+        {/* <p>{props.item.description}</p> */}
+        <p className='price'>
+          <b>from ${props.item.price} /</b> person
+        </p>
+        <p className='spots'></p>
+      </div>
+    </section>
+    // </div>
+  );
+};
+export default Card;
